acc60d6ce8dcc3cfe9af073f1a6c3195e1bc9c90
Francesco Sciabbarrasi <francesco.sciabbarrasii@gmail.com>
2023-09-19 09:30:28 +0100

    beverage section working

--- src/routes/Beverage.js
+++ src/routes/Beverage.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+
 
 
 const cache = {};
@@ -10,10 +14,6 @@ const client = axios.create({
 export default function Beverage() {
     const [beverages, setBeverages] = useState([]);
 
-
-    
-    
-
   useEffect(() => {
     if (cache["beverages"]) {
         setBeverages(cache["beverages"]);
@@ -21,8 +21,8 @@ export default function Beverage() {
         client.get("/recipes/complexSearch?apiKey=8f5c95ab5ba54f428feb304dac547182&type=beverage")
         .then(response => {
         //handle success
-        cache["beverages"] = response.data;
-        setBeverages(response.data)
+        cache["beverages"] = response.data.results;
+        setBeverages(response.data.results)
         })
         .catch(function(error) {
         // handle error
@@ -34,6 +34,19 @@ export default function Beverage() {
         }
     }, [])
     return (
-        console.log(beverages)
+        <CardGroup style={{display:"flex", flexDirection:"row", width:"100vw", overflow:"scroll"}}>
+        {beverages.map((beverage) => (
+            <Card style={{flex: "0 0 auto", maxWidth: "50%"}} key={beverage.id}>
+            <Card.Img variant="top" src={beverage.image} />
+            <Card.Body>
+              <Card.Title>{beverage.title}</Card.Title>
+              <Card.Text>
+                {}
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card> 
+        ))}
+        </CardGroup>
     )
 }
