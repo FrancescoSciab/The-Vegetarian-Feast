62160d0e0566752f128f1c94be01820880469836
Francesco Sciabbarrasi <francesco.sciabbarrasii@gmail.com>
2023-09-20 14:15:17 +0100

    will fix active status later

--- src/components/FoodMenu.js
+++ src/components/FoodMenu.js
@@ -1,15 +1,21 @@
 import Pagination from 'react-bootstrap/Pagination';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 
 
 function FoodMenu() {
+  const [active, setActive] = useState(true)
+  
+    
+  
+  
   
     return (
       <Pagination>
         
-        <Pagination.Item active>Food</Pagination.Item>
+        <Pagination.Item  active><Link to={"/"}>Food</Link></Pagination.Item>
         <Pagination.Item><Link to={"/beverage"}>Beverage</Link></Pagination.Item>
         <Pagination.Item><Link to={"/dessert"}>Dessert</Link></Pagination.Item>
         
