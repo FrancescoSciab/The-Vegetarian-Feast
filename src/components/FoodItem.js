5cb68156501de8243b9c0ef7666e9fd5f9025574
Francesco Sciabbarrasi <francesco.sciabbarrasii@gmail.com>
2023-09-23 11:12:27 +0100

    double link warning solved by implementing react bootstrap cards instead

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -1,6 +1,8 @@
-import Pagination from 'react-bootstrap/Pagination';
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/Card';
 // API call to be moved to root component (?)
 
 
@@ -9,15 +11,24 @@ function FoodItem(props) {
     return (
         // It only runs when dishtypes is not null
         //Will solve double link issue once I decide what to choose instead of "Pagination.Item"
-      <Pagination>
+      
 
-        
-
-            <Pagination.Item><Link to={"/lunch"}>Lunch</Link></Pagination.Item>
-            
-        
-        
-      </Pagination>
+<>
+<CardGroup>
+    
+        <Card>
+        <Card.Img variant="top" />
+        <Card.Body>
+          <Card.Title>Lunch</Card.Title>
+          <Card.Text>
+            Check out our new lunch ideas
+          </Card.Text>
+          <Button variant="primary"><Link to={"lunch"}>Go Somewhere</Link></Button>
+        </Card.Body>
+      </Card> 
+    
+</CardGroup>
+</>
     );
   }
   
