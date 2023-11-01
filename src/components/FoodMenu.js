7c80b91f5e668664cc71c42520ef37e0a9859491
Francesco Sciabbarrasi <francesco.sciabbarrasii@gmail.com>
2023-11-01 11:48:11 +0000

    I might not need FoodMenu component anymore

--- src/components/FoodMenu.js
+++ src/components/FoodMenu.js
@@ -11,40 +11,13 @@ import ToggleButton from 'react-bootstrap/ToggleButton';
 
 
 function FoodMenu() {
-  const [checked, setChecked] = useState(false);
-  const [radioValue, setRadioValue] = useState('1');
   
-    const radios = [
-      { name: 'Food', value: '1', link: '/' },
-      { name: 'Beverage', value: '2', link: 'beverage' },
-      { name: 'Dessert', value: '3', link: 'dessert' },
-    ];
   
     return (
 
       <>
-
-        <ButtonGroup>
-          {radios.map((radio, idx) => (
-            <Link to={radio.link} ><ToggleButton
-            key={idx}
-            id={`radio-${idx}`}
-            type="radio"
-            variant={idx % 2 ? 'outline-success' : 'outline-danger'}
-            name="radio"
-            value={radio.value}
-            checked={radioValue === radio.value}
-            onChange={(e) => setRadioValue(e.currentTarget.value)}
-          >
-            {radio.name}
-          </ToggleButton></Link>
-          ))}
-        </ButtonGroup>
       </>
-      
-        
-        
-    );
+    )
   }
   
   export default FoodMenu;
