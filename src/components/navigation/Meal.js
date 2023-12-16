016349e76e79b9dd901fe7926ed45a86dabe44ef
Francesco Sciabbarrasi <francesco.sciabbarrasii@gmail.com>
2023-12-16 16:02:01 +0000

    hiding apiKey

--- src/components/navigation/Meal.js
+++ src/components/navigation/Meal.js
@@ -1,153 +1,140 @@
-import { useEffect, useState, Suspense } from 'react';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/Card';
-import { Link, Route, Routes, useParams } from 'react-router-dom';
-import Ingredients from './Ingredients';
+import { useEffect, useState, Suspense } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/Card";
+import { Link, Route, Routes, useParams } from "react-router-dom";
+import Ingredients from "./Ingredients";
 import { Animate } from "react-simple-animate";
-import ErrorPage from '../../error-page';
-import Placeholder from 'react-bootstrap/Placeholder';
-
-
+import ErrorPage from "../../error-page";
+import Placeholder from "react-bootstrap/Placeholder";
 
 const cache = {};
 
 export default function Meal(props) {
+  const { mealType } = useParams();
+  const [meals, setMeals] = useState({
+    loading: true,
+    response: [],
+    errorCode: null,
+  }); //when mealtype changes the api call will be triggered
 
-    
-    const { mealType } = useParams();
-    const [meals, setMeals] = useState({
-      loading: true,
-      response: [],
-      errorCode: null
-    });//when mealtype changes the api call will be triggered
-    
   useEffect(() => {
-    
     if (cache[mealType]) {
       setMeals({
         loading: false,
         response: cache[mealType],
-        errorCode: null
-      })
-  } else {
-      props.client.get(`/recipes/random?apiKey=8f5c95ab5ba54f428feb304dac547182&tags=${mealType}&number=100`)
-      .then(response => {
-      //handle success
-      cache[mealType] = response.data.recipes;
-      setMeals({
-        loading: false,
-        response: response.data.recipes,
-        errorCode: null
-      })
-      })
-      .catch(error => {
-      // the error is gonna be loaded below before rendering
-        setMeals({
-          loading: false,
-          response: null,
-          errorCode: error.request.status
-        })
-      })
-      .finally(function() {
-      // always executed 
+        errorCode: null,
       });
-      }
-    }, [mealType, props.client])
+    } else {
+      props.client
+        .get(`/recipes/random?tags=${mealType}&number=100`)
+        .then((response) => {
+          //handle success
+          cache[mealType] = response.data.recipes;
+          setMeals({
+            loading: false,
+            response: response.data.recipes,
+            errorCode: null,
+          });
+        })
+        .catch((error) => {
+          // the error is gonna be loaded below before rendering
+          setMeals({
+            loading: false,
+            response: null,
+            errorCode: error.request.status,
+          });
+        })
+        .finally(function () {
+          // always executed
+        });
+    }
+  }, [mealType, props.client]);
 
-
-    return (
-      
-      <>
-      {meals.errorCode ? <ErrorPage errorStatus={meals.errorCode}/> : null}
+  return (
+    <>
+      {meals.errorCode ? <ErrorPage errorStatus={meals.errorCode} /> : null}
       <Routes>
-
-<Route path="*" element={
-  
-        meals.loading 
-        ? 
-        (
-          <CardGroup id="card-group-placeholder">
-            {Array.from({ length: 12 }).map((_, index) => (
-              <Card id="placeholder-card" key={`placeholder-${index}`}>
-                <Card.Body id="placeholder-card-body">
-                  <Placeholder as={Card.Title} animation="glow">
-                    <Placeholder xs={6} />
-                  </Placeholder>
-                  <Placeholder as={Card.Text} animation="glow">
-                    <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
-                    <Placeholder xs={6} /> <Placeholder xs={8} />
-                    <Placeholder.Button variant="primary" xs={6} />
-                  </Placeholder>
-                </Card.Body>
-              </Card>
-            ))}
-          </CardGroup>
-        )
-       
-        :
-
-        (
-          <CardGroup id="card-group-meal">
-            {meals.response && meals.response.map((meal, index) => (
-    
-              <Animate play 
-              key={meal}
-              sequenceIndex={index}
-              start={{ opacity: 0, transform: 'translateX(20px)' }}
-              end={{ opacity: 1, transform: 'translateX(0)' }}>
-                <Card id="card-meal" key={meal.id} >
-                <Card.Img 
-                  id="card-img-meal" 
-                  variant="top" 
-                  src={meal.image} />
-                <Card.Body  id="card-body-meal">
-        
-                  <Card.Title>{meal.title}</Card.Title>
-                  <Card.Subtitle>
-                    <div id="recipe-card-info-container">
+        <Route
+          path="*"
+          element={
+            meals.loading ? (
+              <CardGroup id="card-group-placeholder">
+                {Array.from({ length: 12 }).map((_, index) => (
+                  <Card id="placeholder-card" key={`placeholder-${index}`}>
+                    <Card.Body id="placeholder-card-body">
+                      <Placeholder as={Card.Title} animation="glow">
+                        <Placeholder xs={6} />
+                      </Placeholder>
+                      <Placeholder as={Card.Text} animation="glow">
+                        <Placeholder xs={7} /> <Placeholder xs={4} />{" "}
+                        <Placeholder xs={4} /> <Placeholder xs={6} />{" "}
+                        <Placeholder xs={8} />
+                        <Placeholder.Button variant="primary" xs={6} />
+                      </Placeholder>
+                    </Card.Body>
+                  </Card>
+                ))}
+              </CardGroup>
+            ) : (
+              <CardGroup id="card-group-meal">
+                {meals.response &&
+                  meals.response.map((meal, index) => (
+                    <Animate
+                      play
+                      key={meal}
+                      sequenceIndex={index}
+                      start={{ opacity: 0, transform: "translateX(20px)" }}
+                      end={{ opacity: 1, transform: "translateX(0)" }}
+                    >
+                      <Card id="card-meal" key={meal.id}>
+                        <Card.Img
+                          id="card-img-meal"
+                          variant="top"
+                          src={meal.image}
+                        />
+                        <Card.Body id="card-body-meal">
+                          <Card.Title>{meal.title}</Card.Title>
+                          <Card.Subtitle>
+                            <div id="recipe-card-info-container">
                               <span id="recipe-card-info-span">
-                              &#x1F550; {meal.readyInMinutes}
-                                <span>Minutes</span> 
+                                &#x1F550; {meal.readyInMinutes}
+                                <span>Minutes</span>
                               </span>
 
                               <span id="recipe-card-info-span">
-                              &#x1F464; {meal.servings}
-                                <span>Servings</span> 
+                                &#x1F464; {meal.servings}
+                                <span>Servings</span>
                               </span>
 
                               <span id="recipe-card-info-span">
-                              &#129382; {meal.healthScore}
-                                <span>HealthScore</span> 
+                                &#129382; {meal.healthScore}
+                                <span>HealthScore</span>
                               </span>
-                    </div>
-                  </Card.Subtitle>
-                  <Card.Text>
-                  </Card.Text>
-                  <Button variant="primary"><Link to={`overview/${meal.id}`}>View Recipe</Link></Button>
-        
-                </Card.Body>
-              </Card>
-              </Animate> 
-            ))}
-            </CardGroup>
+                            </div>
+                          </Card.Subtitle>
+                          <Card.Text></Card.Text>
+                          <Button variant="primary">
+                            <Link to={`overview/${meal.id}`}>View Recipe</Link>
+                          </Button>
+                        </Card.Body>
+                      </Card>
+                    </Animate>
+                  ))}
+              </CardGroup>
             )
-   } />
+          }
+        />
 
-<Route 
-path='overview/:id' 
-element={
-<Suspense fallback={<div>Loading...</div>}>
-  <Ingredients 
-          client={props.client}
-          mealType={mealType}
-          />
-          </Suspense>
-          } 
-          />
-
-</Routes>
-      
-      
-      </>)
+        <Route
+          path="overview/:id"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Ingredients client={props.client} mealType={mealType} />
+            </Suspense>
+          }
+        />
+      </Routes>
+    </>
+  );
 }
--- src/components/navigation/SimilarRecipes.js
+++ src/components/navigation/SimilarRecipes.js
@@ -1,88 +1,92 @@
 import { useEffect, useState } from "react";
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 import { Animate } from "react-simple-animate";
 
-
-const cache = {}
+const cache = {};
 
 export default function SimilarRecipes(props) {
+  const id = props.id;
+  const [similarRecipes, setSimilarRecipes] = useState({
+    loading: true,
+    response: [],
+    errorCode: null,
+  });
 
-    const id = props.id;
-    const [similarRecipes, setSimilarRecipes] = useState({
-      loading: true,
-      response: [],
-      errorCode: null
-    });
+  useEffect(() => {
+    if (cache[id]) {
+      setSimilarRecipes({
+        loading: false,
+        response: cache[id],
+        errorCode: null,
+      });
+    } else {
+      props.client
+        .get(`/recipes/${id}/similar?number=100`)
+        .then((response) => {
+          //handle success
+          cache[id] = response.data;
+          setSimilarRecipes({
+            loading: false,
+            response: response.data,
+            errorCode: null,
+          });
+        })
+        .catch(function (error) {
+          // the error is gonna be loaded below before rendering
+          setSimilarRecipes({
+            loading: false,
+            response: null,
+            errorCode: error.request.status,
+          });
+        })
+        .finally(function () {
+          // always executed
+        });
+    }
+  }, [id, props.client]);
+  return (
+    <>
+      <CardGroup id="similar-recipes-card-group">
+        {similarRecipes.response.map((similarRecipe) => (
+          <Animate
+            play
+            start={{ opacity: 0 }}
+            end={{ opacity: 1 }}
+            duration={0.75}
+          >
+            <Card id="card-meal" key={similarRecipe.id}>
+              <Card.Body style={{ borderRadius: "0.75rem" }}>
+                <Card.Title>{similarRecipe.title}</Card.Title>
+                <Card.Subtitle>
+                  <div id="recipe-card-info-container">
+                    <span id="recipe-card-info-span">
+                      &#x1F550; {similarRecipe.readyInMinutes}
+                      <span>Minutes</span>
+                    </span>
 
-    useEffect(() => {
-        if (cache[id]) {
-            setSimilarRecipes({
-              loading: false,
-              response: cache[id],
-              errorCode: null
-            });
-        } else {
-            props.client.get(`/recipes/${id}/similar?apiKey=8f5c95ab5ba54f428feb304dac547182&number=100`)
-            .then(response => {
-            //handle success
-            cache[id] = response.data;
-            setSimilarRecipes({
-              loading: false,
-              response: response.data,
-              errorCode: null
-            })
-            })
-            .catch(function(error) {
-              // the error is gonna be loaded below before rendering
-              setSimilarRecipes({
-                loading: false,
-                response: null,
-                errorCode: error.request.status
-              })
-            })
-            .finally(function() {
-            // always executed 
-            });
-            }
-        }, [id, props.client])
-    return(
-<>
-
-<CardGroup id="similar-recipes-card-group">
-{similarRecipes.response.map((similarRecipe) => (
-<Animate play 
-    start={{ opacity: 0 }} 
-    end={{ opacity: 1 }} 
-    duration={0.75}>
-    <Card id="card-meal" key={similarRecipe.id}>
-    <Card.Body 
-    style={{borderRadius: "0.75rem"}}>
-      <Card.Title>{similarRecipe.title}</Card.Title>
-      <Card.Subtitle>
-      <div id="recipe-card-info-container">
-                <span id="recipe-card-info-span">
-                &#x1F550; {similarRecipe.readyInMinutes}
-                  <span>Minutes</span> 
-                </span>
-
-                <span id="recipe-card-info-span">
-                &#x1F464; {similarRecipe.servings}
-                  <span>Servings</span> 
-                </span>
-      </div>
-        
-        </Card.Subtitle>
-      <Card.Text>
-      </Card.Text>
-      <Button variant="primary"><Link to={`/${props.mealType}/overview/${similarRecipe.id}`} replace>View Recipe</Link></Button>
-    </Card.Body>
-  </Card> 
-</Animate>
-))}
-</CardGroup>
-</>
-    )
+                    <span id="recipe-card-info-span">
+                      &#x1F464; {similarRecipe.servings}
+                      <span>Servings</span>
+                    </span>
+                  </div>
+                </Card.Subtitle>
+                <Card.Text></Card.Text>
+                <Button variant="primary">
+                  <Link
+                    to={`/${props.mealType}/overview/${similarRecipe.id}`}
+                    replace
+                  >
+                    View Recipe
+                  </Link>
+                </Button>
+              </Card.Body>
+            </Card>
+          </Animate>
+        ))}
+      </CardGroup>
+    </>
+  );
 }
