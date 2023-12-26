9fe199c9f31badc343f9bc2a38ec4f6d2aecfade
Francesco Sciabbarrasi <francesco.sciabbarrasii@gmail.com>
2023-12-26 10:10:15 +0100

    better mealtype scrolling

--- src/components/navbar/SearchBar.js
+++ src/components/navbar/SearchBar.js
@@ -2,41 +2,35 @@ import { useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
 import { useNavigate } from "react-router-dom";
 
-
-
 export default function SearchItems(props) {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
 
-    
-    const [query, setQuery] = useState("");
-    const navigate = useNavigate();
+  const handleChange = (event) => {
+    setQuery(event.target.value);
+  };
 
-    const handleChange = event => {
-        setQuery(event.target.value); 
-    }
+  //no need to cache
+  useEffect(() => {
+    navigate(query);
+  }, [query]);
 
-    //no need to cache
-    useEffect(() => {
-        navigate(query); 
-}, [query])
-    
-    
-    return (
-        <>
-            <Form style={{width: "100%", marginRight: "16px"}}>
-                <Form.Group>
-                    <Form.Label>Type your search query</Form.Label>
-                    <Form.Control 
-                        type="search" 
-                        placeholder="Search" 
-                        controlid="header-search"
-                        name="query"
-                        value={query}
-                        onChange={handleChange}
-                    />
-                </Form.Group>
-            </Form>
-        </>
-        
-    )
+  return (
+    <>
+      <Form style={{ width: "100%", marginRight: "16px" }}>
+        <Form.Group>
+          <Form.Label>Type your search query</Form.Label>
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            controlid="header-search"
+            name="query"
+            value={query}
+            onChange={handleChange}
+            autoFocus
+          />
+        </Form.Group>
+      </Form>
+    </>
+  );
 }
-
