94af1dff9213c83402817dcc701b32e2706bd4cc
Francesco Sciabbarrasi <francesco.sciabbarrasii@gmail.com>
2024-04-18 17:30:44 +0200

    centering error message

--- src/error-page.js
+++ src/error-page.js
@@ -1,17 +1,20 @@
 import Col from "react-bootstrap/esm/Col";
+import Row from "react-bootstrap/esm/Row";
 
 export default function ErrorPage(props) {
   const error = props.errorStatus;
 
   return (
-    <Col md={8}>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        {error === 402
-          ? "Reached API calls limit, payment required. You can still check out the latest tik tok recipes below."
-          : "Error"}
-      </p>
-    </Col>
+    <Row>
+      <Col md={8}>
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          {error === 402
+            ? "Reached API calls limit, payment required. You can still check out the latest tik tok recipes below."
+            : "Error"}
+        </p>
+      </Col>
+    </Row>
   );
 }
